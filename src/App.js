dc6d2483b4326b77384a196e94d5cc7f6e9d6bc7
Dom Waterson <dom_waterson@hotmail.com>
2020-09-25 19:54:49 +0100

    added jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import jumboData from "./fixtures/jumbo.json";
+import Jumbotron from "./components/jumbotron";
 
 function App() {
-  return <h1>Netflix clone</h1>;
+  return (
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
 }
 
 export default App;
