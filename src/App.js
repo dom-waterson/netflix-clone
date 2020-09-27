f8d7eac97938e863605ae9bbf763a043525dbc57
Dom Waterson <dom_waterson@hotmail.com>
2020-09-27 16:36:34 +0100

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import * as ROUTES from "./constants/routes";
+import { Home, Browse, Signin, Signup } from "./pages";
 
 function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
+    </Router>
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { FaqsContainer } from "../containers/faqs";
+import { FooterContainer } from "../containers/footer";
+import { JumbotronContainer } from "../containers/jumbotron";
+
+export default function Home() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
+}
