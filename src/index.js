f36f22c531130b789312eae7cabf2f59b9fa5336
Dom Waterson <dom_waterson@hotmail.com>
2020-09-28 19:34:32 +0100

    added firebase connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
+
 import App from "./app";
 import { GlobalStyles } from "./global-styles";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context";
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById("root")
 );
