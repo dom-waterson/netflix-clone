4c18577c4e4e319a98236f1023571c1c508c24a3
Dom Waterson <dom_waterson@hotmail.com>
2020-09-28 20:28:55 +0100

    updated sign in page to include form component

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FooterContainer, HeaderContainer } from "../containers";
+import { Form } from "../components";
 
 export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
-      <HeaderContainer>SIGN IN</HeaderContainer>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              autoComplete="off"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            <Form.Link to="/signup">New to Netflix?</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
       <FooterContainer />
     </>
   );
