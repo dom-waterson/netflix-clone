428bfdd24273507d4826e3edd6b247686592ba7b
Dom Waterson <dom_waterson@hotmail.com>
2020-09-28 21:26:30 +0100

    added firebase connection to sign up page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -19,6 +19,26 @@ export default function Signup() {
 
   const handleSignup = (event) => {
     event.preventDefault();
+
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          })
+      )
+      .catch((error) => {
+        setFirstName("");
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
   };
 
   return (
