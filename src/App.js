71196f20f8b12866a83b34c5921db6bedcea73f5
Dom Waterson <dom_waterson@hotmail.com>
2020-09-29 20:24:04 +0100

    added auth hook

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 function App() {
-  const user = null;
+  const user = useAuthListener();
+  console.log(user);
   return (
     <Router>
       <Switch>
