237c74dcfecf88150ba8026f776b3d0db9ac94ae
Dom Waterson <dom_waterson@hotmail.com>
2020-10-01 19:27:52 +0100

    added selection filter util for browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useContent } from "../hooks";
+import { selectionFilter } from "../utils";
 
 export default function Browse() {
   const { series } = useContent("series");
   const { films } = useContent("films");
-  console.log(series);
-  console.log(films);
+
+  const sildes = selectionFilter({ series, films });
+
   return <p>Browse</p>;
 }
