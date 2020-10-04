ea66926546fcd5794b93ff7d1877dfab900b988f
Dom Waterson <dom_waterson@hotmail.com>
2020-10-04 16:58:07 +0100

    implemented search with fuse.js

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import Fuse from "fuse.js";
 import { Card, Header, Loading, Player } from "../components";
 import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
@@ -15,6 +16,21 @@ export default function BrowseContainer({ slides }) {
 
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data,description", "data.genre", "data.title"],
+    });
+
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchTerm]);
 
   useEffect(() => {
     setTimeout(() => {
